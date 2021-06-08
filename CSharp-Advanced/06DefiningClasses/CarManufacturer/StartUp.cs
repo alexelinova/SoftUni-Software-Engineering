4ab8db4c3e731fbd3d6bac7bfb02a4b391e17541
Alex <81240373+alexelinova@users.noreply.github.com>
2021-06-08 16:30:04 +0300

    Addin Defining Classes - Lab

---
+++ CSharp-Advanced/06DefiningClasses/CarManufacturer/StartUp.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            List<Tire[]> tires = new List<Tire[]>();
+            List<Engine> engines = new List<Engine>();
+            List<Car> cars = new List<Car>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "No more tires")
+                {
+                    break;
+                }
+
+                string[] tiresInfo = input.Split();
+
+                Tire[] currTires = new Tire[4]
+                {
+                    new Tire(int.Parse(tiresInfo[0]), double.Parse(tiresInfo[1])),
+                    new Tire(int.Parse(tiresInfo[2]), double.Parse(tiresInfo[3])),
+                    new Tire(int.Parse(tiresInfo[4]), double.Parse(tiresInfo[5])),
+                    new Tire(int.Parse(tiresInfo[6]), double.Parse(tiresInfo[7])),
+                };
+
+                tires.Add(currTires);
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Engines done")
+                {
+                    break;
+                }
+
+                string[] enginesInfo = input.Split();
+
+                Engine currEngine = new Engine(int.Parse(enginesInfo[0]), double.Parse(enginesInfo[1]));
+
+                engines.Add(currEngine);
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Show special")
+                {
+                    break;
+                }
+
+                string[] carsInfo = input.Split();
+
+                string make = carsInfo[0];
+                string model = carsInfo[1];
+                int year = int.Parse(carsInfo[2]);
+                double fuelQuantity = double.Parse(carsInfo[3]);
+                double fuelConsumption = double.Parse(carsInfo[4]);
+                int engineIndex = int.Parse(carsInfo[5]);
+                int tiresIndex = int.Parse(carsInfo[6]);
+
+                Car currCar = new Car(make, model, year, fuelQuantity, fuelConsumption, engines[engineIndex], tires[tiresIndex]);
+
+                cars.Add(currCar);
+            }
+            List<Car> specialCars = GetSpecialCars(cars);
+
+            double distance = 20;
+
+            foreach (var car in specialCars)
+            {
+                car.Drive(distance);    
+            }
+
+            PrintSpecialCars(specialCars);
+        }
+
+        private static void PrintSpecialCars(List<Car> specialCars)
+        {
+            foreach (var car in specialCars)
+            {
+                Console.WriteLine($"Make: {car.Make}");
+                Console.WriteLine($"Model: {car.Model}");
+                Console.WriteLine($"Year: {car.Year}");
+                Console.WriteLine($"HorsePowers: {car.Engine.HorsePower}");
+                Console.WriteLine($"FuelQuantity: {car.FuelQuantity}");
+            }
+        }
+
+        private static List<Car> GetSpecialCars(List<Car> cars)
+        {
+            return cars
+                .Where(x => x.Year >= 2017)
+                .Where(x => x.Engine.HorsePower > 330)
+                .Where(x => x.Tires.Sum(x => x.Pressure) >= 9 && x.Tires.Sum(x => x.Pressure) <= 10).ToList();
+        }
+    }
+}
