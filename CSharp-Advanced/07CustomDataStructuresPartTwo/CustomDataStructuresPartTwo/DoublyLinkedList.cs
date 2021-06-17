7dd2f24a2c2982eb253bdaf9b50cd7503c0c0004
Alex <81240373+alexelinova@users.noreply.github.com>
2021-06-17 14:34:50 +0300

    Adding Custom Data Structures - Part Two

---
+++ CSharp-Advanced/07CustomDataStructuresPartTwo/CustomDataStructuresPartTwo/DoublyLinkedList.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDoublyLinkedList
+{
+    public class DoublyLinkedList<T>
+    {
+        private class ListNode
+        {
+            public ListNode(T value)
+            {
+                this.Value = value;
+            }
+
+            public ListNode NextNode { get; set; }
+            public ListNode PreviousNode { get; set; }
+            public T Value { get; set; }
+        }
+
+        private ListNode head;
+
+        private ListNode tail;
+        public int Count { get; private set; }
+
+        public void AddFirst(T element)
+        {
+            ListNode newHead = new ListNode(element);
+
+            if (this.Count == 0)
+            {
+                this.head = this.tail = newHead;
+            }
+            else
+            {
+                newHead.NextNode = this.head;
+                this.head.PreviousNode = newHead;
+                this.head = newHead;
+            }
+
+            this.Count++;
+        }
+
+        public void AddLast(T element)
+        {
+            ListNode newTail = new ListNode(element);
+
+            if (this.Count == 0)
+            {
+                this.head = this.tail = newTail;
+            }
+            else
+            {
+                newTail.PreviousNode = this.tail;
+                this.tail.NextNode = newTail;
+                this.tail = newTail;
+            }
+
+            this.Count++;
+        }
+
+        public T RemoveFirst()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("DoubleLinkedList is empty");
+            }
+
+            T firstElement = this.head.Value;
+
+            ListNode newHead = this.head.NextNode;
+            this.head = newHead;
+
+            if (this.head != null)
+            {
+                this.head.PreviousNode = null;
+            }
+            else
+            {
+                this.tail = null;
+            }
+
+            this.Count--;
+            return firstElement;
+        }
+
+        public T RemoveLast()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("DoubleLinkedList is empty");
+            }
+            T removedElement = this.tail.Value;
+
+            ListNode newTail = this.tail.PreviousNode;
+
+            if (this.Count == 1)
+            {
+                this.tail = this.head = null;
+            }
+            else
+            {
+                newTail.NextNode = null;
+                this.tail = newTail;
+            }
+            this.Count--;
+            return removedElement;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            ListNode currentNode = this.head;
+
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        public void Clear()
+        {
+            this.head = this.tail = null;
+            Count = 0;
+        }
+
+        public T[] ToArray()
+        {
+            T[] array = new T[this.Count];
+
+            ListNode currentNode = this.head;
+
+            int counter = 0;
+
+            while (currentNode != null)
+            {
+                array[counter] = currentNode.Value;
+                counter++; 
+               currentNode = currentNode.NextNode;
+            }
+
+            return array;
+        }
+    }
+}
---
+++ CSharp-Advanced/07CustomDataStructuresPartTwo/CustomDataStructuresPartTwo/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CustomDoublyLinkedList
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            DoublyLinkedList<int> linkedlist = new DoublyLinkedList<int>();
+
+            linkedlist.AddFirst(1);
+        }
+    }
+}
