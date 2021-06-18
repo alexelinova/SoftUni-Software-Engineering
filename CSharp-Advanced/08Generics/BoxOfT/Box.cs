a395e0b5376ce7b637c2913c92f759f28902a764
Alex <81240373+alexelinova@users.noreply.github.com>
2021-06-18 13:09:03 +0300

    Adding Generics - Lab

---
+++ CSharp-Advanced/08Generics/BoxOfT/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        List<T> collection;
+
+        public Box()
+        {
+            this.collection = new List<T>();
+        }
+        public int Count => this.collection.Count;
+
+        public void Add(T element)
+        {
+            this.collection.Add(element);
+        }
+
+        public T Remove()
+        {
+            if (this.collection.Count == 0)
+            {
+                throw new InvalidOperationException("The box is empty");
+            }
+
+            T removed = this.collection[collection.Count - 1];
+            this.collection.RemoveAt(collection.Count - 1);
+
+            return removed;
+        }
+    }
+}
