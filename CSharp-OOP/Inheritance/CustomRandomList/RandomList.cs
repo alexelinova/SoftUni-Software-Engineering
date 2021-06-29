a2dd7df3d5db0156b2a80998eda8271d24bf3207
Alex <81240373+alexelinova@users.noreply.github.com>
2021-06-29 13:35:32 +0300

    Adding Inheritance - Lab

---
+++ CSharp-OOP/Inheritance/CustomRandomList/RandomList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        Random rnd;
+        public RandomList()
+        {
+            rnd = new Random();
+        }
+        public string RandomString()
+        {
+            int index = rnd.Next(0, Count);
+            string str = this[index];
+            this.RemoveAt(index);
+
+            return str;
+        }
+    }
+}
