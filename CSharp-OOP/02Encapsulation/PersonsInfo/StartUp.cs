20f61aa6edaf4fc9c0ddf4e4e684357ed8e4adb6
Alex <81240373+alexelinova@users.noreply.github.com>
2021-07-02 13:00:23 +0300

    Adding Encapsulation - Lab

---
+++ CSharp-OOP/02Encapsulation/PersonsInfo/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                try
+                {
+                    Person person = new Person(input[0], input[1], int.Parse(input[2]), decimal.Parse(input[3]));
+                    people.Add(person);
+                }
+                catch (ArgumentException ae)
+                {
+
+                    Console.WriteLine(ae.Message);
+                }
+                
+            }
+
+            Team team = new Team("SoftUni");
+
+            foreach (var person in people)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+
+
+        }
+    }
+}
